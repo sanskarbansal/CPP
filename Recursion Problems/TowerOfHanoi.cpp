815ae05ead6ce9f278c1abaf4920c0c88d827f39
Sanskar Bansal <32870271+sanskarbansal@users.noreply.github.com>
2018-09-23 10:22:04 +0530

    Recursion(Problems)

---
+++ Recursion Problems/TowerOfHanoi.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+  if(n == 0)
+    return;
+  if(n == 1){
+    cout << source << " " << destination << endl;
+    return;
+  }
+  towerOfHanoi(n- 1, source, destination , auxiliary);
+  cout << source << " " << destination << endl;
+  towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+int main(){
+    int n;
+    char a, b, c;
+    cin >> n >> a >> b >> c;
+    towerOfHanoi(n, a, b, c);
+    return 0;
+}
